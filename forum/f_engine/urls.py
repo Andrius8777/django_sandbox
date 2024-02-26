0bd468e8eb6b8084cd8cc4e914f61630ba92b415
Andrius8777 <andrius001@protonmail.com>
2024-02-26 22:26:19 +0200

    home page

--- forum/f_engine/urls.py
+++ forum/f_engine/urls.py
@@ -3,8 +3,11 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('w_engine/', views.thread_list, name='thread_list'),                #def task_list
-    path('w_engine/<int:pk>/', views.thread_detail, name='thread_detail'),    #def task_detail
+    #path('w_engine/', views.thread_list, name='thread_list'),                #def task_list
+    #path('w_engine/<int:pk>/', views.thread_detail, name='thread_detail'),    #def task_detail
     path('w_engine/post/', views.post_list, name='post_list'),                #def post_list
-    path('w_engine/post/<int:pk>/', views.post_detail, name='post_detail'),    #def post_detail
+    path('w_engine/post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('threads/', views.ThreadListView.as_view(), name='thread_list'),
+    path('thread/<int:pk>/', views.ThreadDetailView.as_view(), name='thread_detail'),
 ]
+
